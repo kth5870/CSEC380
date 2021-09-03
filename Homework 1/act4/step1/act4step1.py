fbb85eab995d924856be2cee56545f4e9c1f738c
Kayla Hodgson <kth5870@rit.edu>
2021-09-03 12:46:28 -0400

    moving files to homework1 folder and finished step1 act4

---
+++ Homework 1/act4/step1/act4step1.py
@@ -0,0 +1,16 @@
+import requests
+"""
+Kayla Hodgson
+CSEC 380 - Homework 1
+Step 1 Act 4
+09/03/2021
+"""
+
+def request_url():
+    request = requests.get("http://csec.rit.edu")
+
+def main():
+    request_url()
+
+if __name__ == "__main__":
+    main()
