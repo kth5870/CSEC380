e5cb3dad2d6e808f484cf092ecf3d81f72f79f42
Kayla Hodgson <kth5870@rit.edu>
2021-09-06 00:19:26 -0400

    pushing starting code for script

---
+++ Homework 1/act4/step2/act4step2.py
@@ -0,0 +1,10 @@
+import sys
+import threading
+
+def scan_for_proxy(start_ip, end_ip):
+    pass
+
+def main():
+    start_ip = sys.argv[1]
+    end_ip = sys.argv[2]
+    scan_for_proxy(start_ip, end_ip)
